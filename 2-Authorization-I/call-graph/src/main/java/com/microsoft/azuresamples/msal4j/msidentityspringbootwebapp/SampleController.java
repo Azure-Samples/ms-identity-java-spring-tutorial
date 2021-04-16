d80c07b549a43c58a3118ea15511f1aac68093c4
idg-sam <66391663+idg-sam@users.noreply.github.com>
2021-04-16 14:47:31 -0700

    call-graph: update controller

--- 2-Authorization-I/call-graph/src/main/java/com/microsoft/azuresamples/msal4j/msidentityspringbootwebapp/SampleController.java
+++ 2-Authorization-I/call-graph/src/main/java/com/microsoft/azuresamples/msal4j/msidentityspringbootwebapp/SampleController.java
@@ -23,16 +23,25 @@ public class SampleController {
     private String content;
 
     /**
+     * Add HTML partial fragment from /templates/content folder to request and serve base html
+     * @param model Model used for placing user param and bodyContent param in request before serving UI.
+     * @param fragment used to determine which partial to put into UI
+     */
+    private String hydrateUI(Model model, String fragment) {
+        model.addAttribute(content, String.format("content/%s.html", fragment));
+        return baseUI;
+    }
+
+    /**
      *  Sign in status endpoint
-     *  The page demonstrates sign-in status. For full details, see the src/main/webapp/content/status.jsp file.
+     *  The page demonstrates sign-in status. For full details, see the src/main/webapp/content/status.html file.
      * 
      * @param model Model used for placing bodyContent param in request before serving UI.
      * @return String the UI.
      */
     @GetMapping(value = {"/", "sign_in_status", "/index"})
     public String status(Model model) {
-        model.addAttribute(content, "content/status.html");
-        return baseUI;
+        return hydrateUI(model, "status");
     }
 
     /**
@@ -47,8 +56,7 @@ public class SampleController {
     @GetMapping(path = "/token_details")
     public String tokenDetails(Model model, @AuthenticationPrincipal OidcUser principal) {
         model.addAttribute("claims", Utilities.filterClaims(principal));
-        model.addAttribute(content, "content/token.html");
-        return baseUI;
+        return hydrateUI(model, "token");
     }
 
     /**
@@ -64,15 +72,14 @@ public class SampleController {
     @GetMapping(path = "/call_graph")
     public String callGraph(Model model, @RegisteredOAuth2AuthorizedClient("graph") OAuth2AuthorizedClient graphAuthorizedClient) {
         model.addAttribute("user", Utilities.graphUserProperties(graphAuthorizedClient));
-        model.addAttribute(content, "content/graph.html");
-        return baseUI;
+        return hydrateUI(model, "graph");
     }
+
 
     // survey endpoint - did the sample address your needs?
     // not an integral a part of this tutorial.
     @GetMapping(path = "/survey")
     public String survey(Model model) {
-        model.addAttribute(content, "content/survey.html");
-        return baseUI;
+        return hydrateUI(model, "survey");
     }
 }
