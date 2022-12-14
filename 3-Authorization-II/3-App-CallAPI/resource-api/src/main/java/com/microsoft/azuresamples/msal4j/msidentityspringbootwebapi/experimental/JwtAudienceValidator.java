296a1cca1839fdc3c59816cfdbfc9954c2d05b6b
Yuhao Yang (Collabera Canada <v-yuhaoyang@microsoft.com>
2022-12-14 15:13:49 -0500

    added 3-CallAppAPI for a second review

---
+++ 3-Authorization-II/3-App-CallAPI/resource-api/src/main/java/com/microsoft/azuresamples/msal4j/msidentityspringbootwebapi/experimental/JwtAudienceValidator.java
@@ -0,0 +1,44 @@
+package com.microsoft.azuresamples.msal4j.msidentityspringbootwebapi.experimental;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimNames;
+import org.springframework.security.oauth2.jwt.JwtClaimValidator;
+import org.springframework.security.oauth2.jwt.JwtIssuerValidator;
+import org.springframework.util.Assert;
+
+
+public final class JwtAudienceValidator implements OAuth2TokenValidator<Jwt> {
+
+	private final JwtClaimValidator<Object> validator;
+
+	public JwtAudienceValidator(String audience) {
+		Assert.notNull(audience, "audience cannot be null");
+		this.validator = new JwtClaimValidator<>(JwtClaimNames.AUD, claimPredicate(audience));
+	}
+
+	@Override
+	public OAuth2TokenValidatorResult validate(Jwt token) {
+		Assert.notNull(token, "token cannot be null");
+		return this.validator.validate(token);
+	}
+	
+    Predicate<Object> claimPredicate(String claim) {
+        return clm -> {
+            if (clm == null) {
+                return false;
+            } else if (clm instanceof String) {
+                return clm.equals(claim);
+            } else if (clm instanceof List) {
+                return ((List<?>) clm).contains(claim);
+            } else {
+                return false;
+            }
+        };
+    }  
+
+}
